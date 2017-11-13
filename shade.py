87350f04a881647b96115badfa6004d0233ed031
Jonah Haney <jdh104@users.noreply.github.com>
2017-11-13 16:19:22 -0600

    Update ver 2017.9.21

--- shade.py
+++ shade.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+Author = "Jonah Haney"
+Version = "2017.9.21"
+
 import os
 import sys
 import argparse
@@ -9,7 +12,7 @@ files = []
 parser = argparse.ArgumentParser()
 key_type_group = parser.add_mutually_exclusive_group(required = True)
 
-key_type_group.add_argument("-p", metavar = "<K>", dest = "p", help = "Use Passphrase <K> as key", default = None)
+key_type_group.add_argument("-p", metavar = "<P>", dest = "p", help = "Use Passphrase <P> as key (relatively vulnerable)", default = None)
 key_type_group.add_argument("-F", metavar = "<K>", dest = "F", help = "Use File <K> as key", default = None)
 key_type_group.add_argument("-G", metavar = "<K>", dest = "G", help = "Generate keyfile named <K>", default = None)
 parser.add_argument("-s", metavar = "<S>", dest = "s", help = "Generate random key of size <S> in bytes (Default: 16) Ignored if -G not used (Note: key sizes higher than 16 illegal for international use [USA])", default = 16, type = int)
@@ -29,6 +32,7 @@ def terminate(exit_message):
 			f.close()
 		except:
 			log("Failed to close file: " + f.name)
+	sys.stderr.flush()
 	quit()
 
 # Open a file, use this in place of open()
@@ -46,72 +50,83 @@ def keystream(key, size):
 	return key * (int(size / len(key))) + key[:(size % len(key))]
 
 # Basic xor crypto, returns array of bytes
+# NOTE that keyfile must be file_2
 def xor_with_file(file_1, file_2):
 	log("\nOperating on " + file_1.name + " ...")
+	
+	# Initialize below loop
 	x, y = file_1.read(1), file_2.read(1)
 	z = fopen(".shade.temp", "wb+")
-	flag1, flag2 = False, False
-	goal = max(os.path.getsize(file_1.name), os.path.getsize(file_2.name))
-	mile = 1
-	i = 0
-	while (not flag1) or (not flag2):
+	len_1, len_2 = os.path.getsize(file_1.name), os.path.getsize(file_2.name)
+	check, addup = 0, 0
+	i, mile = 0, 1
+	goal = max(len_1, len_2)
+	while i < goal:
 		z.write(bytes([ord(x) ^ ord(y)]))
 		i += 1
+
+		# Scrambler Start ######################
+		addup = (addup + ord(y) + 1) % 0x100  ##
+		check = (addup + check) % len_2       ##
+		file_2.seek(check)                    ##
+		# Scrambler End ########################
+
 		x, y = file_1.read(1), file_2.read(1)
-		if (x == b''):
+		if (x == b''): # Should only happen if keysize > opsize
+			overwrite(z, file_1)
 			file_1.seek(0)
 			x = file_1.read(1)
 			z.seek(0)
-			flag1 = True
-		if (y == b''):
+		if (y == b''): # Shouldn't happen
 			file_2.seek(0)
 			y = file_2.read(1)
-			flag2 = True
-		if (i * 100) / goal > mile:
+			log("Unexpected end of keyfile reached, fixed")
+		if (i * 100) / goal > mile: # Progress Logging
 			log(str(i * 100 / goal)[:5] + "%")
 			mile = (i * 100 / goal) + 1
 			sys.stderr.flush()
 	return z
 
-def xor_with_key(op, key):
+# Used when given a passphrase instead of a file (note the plaintext attack vector vulnerability)
+def xor_with_pass(op, key):
 	log("\nOperating on " + op.name + " ...")
 	x = op.read(1)
 	z = fopen(".shade.temp", "wb+")
-	goal = max(os.path.getsize(op.name), len(key))
+	len_1, len_2 = os.path.getsize(op.name), len(key)
+	goal = max(len_1, len_2)
+	check, addup = 0, 0
 	mile = 1
-	i = 0
-	while x != b'':
-		z.write(bytes([ord(x) ^ ord(key[i])]))
+	while i < goal:
+		z.write(bytes([ord(x) ^ ord(key[check])]))
+
+		# Scrambler Start ###############################
+		addup = (addup + ord(key[check]) + 1) % 0x100  ##
+		check = (addup + check) % len_2                ##
+		# Scrambler End #################################
+		
 		x = op.read(1)
 		i += 1
-		if (i * 100) / goal > mile:
+		if (i * 100) / goal > mile: # Progress Logging:
 			log(str(i * 100 / goal)[:5] + "%")
 			mile = (i * 100 / goal) + 1
 			sys.stderr.flush()
+
+		if x == b'': # End of operand file:
+			overwrite(z, op)
+			z.seek(0)
+			op.seek(0)
+			x = op.read(1)
 	return z
 
 # Do not use, irreversible as is
 def shift_bytes(bytes_1, bytes_2):
-	if len(bytes_1) != len(bytes_2):
-		log("Error: shift_bytes not same length")
-		return None
-	else:
-		result = []
-		for i in range(len(bytes_1)):
-			result.append(bytes_1[i] >> bytes_2[i]) # NOT COMPLETE, NEED TO ROTATE
-		return result
+	terminate("Error: shifting not implemented yet") #TODO
 
 # Do not use, irreversible as is
 def unshift_bytes(bytes_1, bytes_2):
-	if len(bytes_1) != len(bytes_2):
-		log("Error: unshift_bytes not same length")
-		return None
-	else:
-		result = []
-		for i in range(len(bytes_1)):
-			result.append(bytes_1[i] << bytes_2[i]) # NOT COMPLETE, NEED TO ROTATE
-		return result
+	terminate("Error: shifting not implemented yet") #TODO
 
+# Write contents of file 'fro' to file 'to'
 def overwrite(fro, to):
 	to.seek(0)
 	fro.seek(0)
@@ -119,12 +134,14 @@ def overwrite(fro, to):
 	while x != b'':
 		to.write(x)
 		x = fro.read(1)
-	
+	to.truncate()
 
+# Generate cryptographically secure random byte stream for key
 def generate_key_file(name, strength):
 	gen = fopen(name, "wb+")
 	for i in range(strength):
-		gen.write(os.urandom(1))
+		last = os.urandom(1)[0]
+		gen.write(bytes([last]))
 	gen.truncate()
 	gen.seek(0)
 	return gen
@@ -157,11 +174,14 @@ for op in args.f:
 	result = None
 	
 	if keyfile != None:
-		result = xor_with_file(operand, keyfile)
+		if operand == None:
+			terminate("No file to convert")
+		else:
+			result = xor_with_file(operand, keyfile)
 	else:
 		key = keystream(key, os.path.getsize(operand.name))
-		result = xor_with_key(operand, key)
+		result = xor_with_pass(operand, key)
 	overwrite(result, operand)
 	os.remove(".shade.temp")
 
-terminate("Successful conversion")
+terminate("Success")
